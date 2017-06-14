49f5930ac5a02f65dd6c22bd8f57c4ba0aa879a4
jayceelock <jaycee.lock@gmail.com>
2017-06-14 08:42:10 +0100

    Deleted useless files, cleaned code

--- app/src/main/java/com/activis/jaycee/virtualcane/ClassTangoUpdateCallback.java
+++ app/src/main/java/com/activis/jaycee/virtualcane/ClassTangoUpdateCallback.java
@@ -46,49 +46,36 @@ class ClassTangoUpdateCallback extends Tango.TangoUpdateCallback
     @Override
     public void onPointCloudAvailable(TangoPointCloudData cloud)
     {
-        /* for(int i = 0; i < cloud.numPoints - 4; i += 4)
-        {
-            if(cloud.points.get(i) * cloud.points.get(i) + cloud.points.get(i + 1) * cloud.points.get(i + 1) < CANE_RADIUS * CANE_RADIUS)
-            {
-                depth = cloud.points.get(i + 2);
-                depthPointPosition.setAll((double)cloud.points.get(i), (double)cloud.points.get(i + 1), -(double)cloud.points.get(i + 2));
-            }
-        }*/
-        try
-        {
-            TangoPoseData oglTdepthPose = TangoSupport.getPoseAtTime(
-                    cloud.timestamp,
-                    TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
-                    TangoPoseData.COORDINATE_FRAME_CAMERA_COLOR,
-                    TangoSupport.TANGO_SUPPORT_ENGINE_OPENGL,
-                    TangoSupport.TANGO_SUPPORT_ENGINE_TANGO,
-                    TangoSupport.ROTATION_IGNORED);
+        TangoPoseData oglTdepthPose = TangoSupport.getPoseAtTime(
+                cloud.timestamp,
+                TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
+                TangoPoseData.COORDINATE_FRAME_CAMERA_COLOR,
+                TangoSupport.TANGO_SUPPORT_ENGINE_OPENGL,
+                TangoSupport.TANGO_SUPPORT_ENGINE_TANGO,
+                TangoSupport.ROTATION_IGNORED);
 
-            TangoPoseData oglTcolorPose = TangoSupport.getPoseAtTime(
-                    activityMain.getRGBTimestamp(),
-                    TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
-                    TangoPoseData.COORDINATE_FRAME_CAMERA_COLOR,
-                    TangoSupport.TANGO_SUPPORT_ENGINE_OPENGL,
-                    TangoSupport.TANGO_SUPPORT_ENGINE_TANGO,
-                    TangoSupport.ROTATION_IGNORED);
+        TangoPoseData oglTcolorPose = TangoSupport.getPoseAtTime(
+                activityMain.getRGBTimestamp(),
+                TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
+                TangoPoseData.COORDINATE_FRAME_CAMERA_COLOR,
+                TangoSupport.TANGO_SUPPORT_ENGINE_OPENGL,
+                TangoSupport.TANGO_SUPPORT_ENGINE_TANGO,
+                TangoSupport.ROTATION_IGNORED);
 
-            float[] point = TangoSupport.getDepthAtPointNearestNeighbor(cloud,
-                    oglTdepthPose.translation,
-                    oglTdepthPose.rotation,
-                    0.5f, 0.5f,
-                    activityMain.getDisplayRotation(),
-                    oglTcolorPose.translation,
-                    oglTcolorPose.rotation);
+        float[] point = TangoSupport.getDepthAtPointNearestNeighbor(cloud,
+                oglTdepthPose.translation,
+                oglTdepthPose.rotation,
+                0.5f, 0.5f,
+                activityMain.getDisplayRotation(),
+                oglTcolorPose.translation,
+                oglTcolorPose.rotation);
             if(point != null)
             {
                 depthPointPosition.setAll((double) point[0], (double) point[1], (double) point[2]);
                 depth = -point[2];
             }
-        }
-        catch(TangoInvalidException e)
-        {
-            Log.e(TAG, "Tango invalid: could not detect point. " + e);
-        }
+
+
         double timestamp = activityMain.getTango().getPoseAtTime(0.0, new TangoCoordinateFramePair(TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE, TangoPoseData.COORDINATE_FRAME_DEVICE)).timestamp;
 
         activityMain.getRunnableVibrate().setDepth(depth);
